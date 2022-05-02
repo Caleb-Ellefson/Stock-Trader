bace3cf703e80c477e1bc521ea9c3304935aa6b0
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-02 19:41:28 +0000

    Mon, May 2, 2022, 7:41 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -7,27 +7,22 @@ int main(void)
 {
     int n = get_int("Height: ");
 
+{
+
 if (n > 8)
+printf("Invaild Height choose a number 1-8.\n");
+{
+    int n = get_int("Height: ");
+
+{
+}
+
+if (n <= 0)
 {
 
 printf("Invaild Height choose a number 1-8.\n");
-
-}
-
-
-
-    // if int is > 8 repromt
-    // if int is < 0 repromt
-    // if int is
-    //if (H < 0 || H > 8)
-
-        {
-            //printf("Invaild Height choose a number 1-8.");
-        }
 {
+    int n = get_int("Height: ");
 
-//printf("Invaild Height choose a number 1-8.\n",(int %h > 8));
-
-}
-
+{
 }
