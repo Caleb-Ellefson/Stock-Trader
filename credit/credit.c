19beb1d647d342e1facbd430f87e062af94cf29f
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-03 17:46:54 +0000

    Tue, May 3, 2022, 5:46 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -11,35 +11,44 @@ int main(void)
         card_number = get_long("Enter your card number here  ");
         card_copy = card_number;
 
-        while (card_number != 0) {
-            if (pos % 2 != 0) {
+        while (card_number != 0)
+        {
+            if (pos % 2 != 0)
+            {
                 int temp = 2 * (card_number % 10);
-                if (temp > 9){
+                if (temp > 9)
+                {
                     total_sum += (temp % 10 + temp / 10);
-                }else {
+                }
+                else
+                {
                     total_sum += temp;
                 }
-            } else {
-                    total_sum += card_number % 10;
-                }
+
+            }
+            else
+            {
+                total_sum += card_number % 10;
+            }
             card_number = card_number / 10;
             pos++;
             total_length++;
         }
     }
-    while(card_number != 0);
+    while (card_number != 0);
 
-    if(total_sum % 10 ==0) {
-    // MasterCard 16 digits starts with 51,52,53,54 or 55
+    if (total_sum % 10 == 0)
+    {
+        // MasterCard 16 digits starts with 51,52,53,54 or 55
         long master_start = card_copy / 10000000000000;
         if ((total_length == 16) && (master_start >= 51 && master_start <= 55))
         {
             printf("MASTERCARD\n");
             return 0;
         }
-    // American Express 15 digits begins with 34 and 37
+        // American Express 15 digits begins with 34 and 37
         long amex_start = card_copy / 1000000000000;
-        if((amex_start == 34 || amex_start == 37) && total_length == 15)
+        if ((amex_start == 34 || amex_start == 37) && total_length == 15)
         {
             printf("AMEX\n");
             return 0;
