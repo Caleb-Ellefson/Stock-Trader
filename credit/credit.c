7d0643f71fb7e7c8e7fa7434c91f2d11075c7cf3
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-03 17:49:44 +0000

    Tue, May 3, 2022, 5:49 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -53,59 +53,17 @@ int main(void)
             printf("AMEX\n");
             return 0;
         }
+        // Visa 13 or 16 digits starts with the number 4
         long visa_start = card_copy / 10000000000000;
         if ((total_length == 13 || total_length == 16) && (visa_start == 4 || master_start / 10 == 4))
         {
             printf("VISA\n");
             return 0;
         }
-
-        // Visa 13 or 16 digits starts with the number 4
-
     }
     else
     {
         printf("INVAILD\n");
     }
-
-
-
-// checksum multiply every other digit by 2 starting with the second to last digit
-
-    // 1234
-    //sum of 1 + 3
-    //1234 % 10 => 4, pos = 0
-    //123 % 10 => 3, pos = 1 => odd
-    //12 % 10 => 2, pos = 2 => even
-    //1 % 10 => 1, pos = 3 => odd
-
-// then add the products of the digits togther
-
-//add the sum of that to the sum of the digits that were not multiplied if the total's last digit = 0 card = vaild
-
-
-// American Express 15 digits begins with 34 and 37
-
-// MasterCard 16 digits starts with 51,52,53,54 or 55
-
-// Visa 13 or 16 digits starts with the number 4
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
