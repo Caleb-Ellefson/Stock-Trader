445c8c1ee0dabf4296bc18ac98f27d0cc606a2da
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-03 03:39:48 +0000

    Tue, May 3, 2022, 3:39 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,7 +1,36 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+
+// American Express 15 digits begins with 34 and 37
+
+// MasterCard 16 digits starts with 51,52,53,54 or 55
+
+// Visa 13 or 16 digits starts with the number 4
+
+// checksum multiply every other digit by 2 starting with the second to last digit
+
+// then add the products of the digits togther
+
+//add the sum of that to the sum of the digits that were not multiplied if the total's last digit = 0 card = vaild
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
