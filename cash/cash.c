f1fa147499874b46a8185fc163fe31c2c64c52b3
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-03 00:06:04 +0000

    Tue, May 3, 2022, 12:06 AM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -53,8 +53,8 @@ int calculate_quarters(int cents)
     int quarters = 0;
     while (cents >= 25);
     {
-       cents = cents - 25;
-       quarters++;
+        cents = cents - 25;
+        quarters++;
     }
     return quarters;
 }
