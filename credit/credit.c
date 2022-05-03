37c0b022aacf84617aee7c48d97ecab17c3e512e
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-03 03:51:19 +0000

    Tue, May 3, 2022, 3:51 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -4,21 +4,28 @@
 int main(void)
 {
     do
+    {
         long 13,15,16 = get_long("Enter your card number here  ")
             else
                 printf("INVALID")
+    }
     while (long == False )
-// American Express 15 digits begins with 34 and 37
 
-// MasterCard 16 digits starts with 51,52,53,54 or 55
 
-// Visa 13 or 16 digits starts with the number 4
 
 // checksum multiply every other digit by 2 starting with the second to last digit
 
 // then add the products of the digits togther
 
 //add the sum of that to the sum of the digits that were not multiplied if the total's last digit = 0 card = vaild
+
+
+// American Express 15 digits begins with 34 and 37
+
+// MasterCard 16 digits starts with 51,52,53,54 or 55
+
+// Visa 13 or 16 digits starts with the number 4
+
 
 
 
