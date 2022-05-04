423dab55f8ffbdd416fab3214da5af40506426db
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-04 01:26:07 +0000

    Wed, May 4, 2022, 1:26 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -20,37 +20,10 @@ int main(void)
 
 }
 
-
-
-
-
-
-
-int count_letters(string text)
-{
-    // Initial number of letters is zero
-    int letterCount = 0;
-
-    // For statement that loops through the text to count the number of letters
-    for (int i = 0; i < strlen(text); i++)
-    {
-        // Increases the letterCount variable if i is an alphabet
-        if (isalpha(text[i]) != 0)
-        {
-            letterCount++;
-        }
-    }
-    return letterCount;
-}
-
-
-
-
-
 // a string of text, and that returns an int, the number of letters in that text.
 int count_letters(string t)
 {
-    int letter_count = 0
+    int letter_count = 0;
 
     for (int i = 0; i < strlen(t); i++)
     {
@@ -58,5 +31,6 @@ int count_letters(string t)
         {
             letter_count++;
         }
+    return letter_count;
     }
 }
