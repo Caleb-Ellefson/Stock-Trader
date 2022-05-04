3b89f942cd17160e93759c06af28e29d45852e65
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-04 01:11:12 +0000

    Wed, May 4, 2022, 1:11 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,6 +1,9 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+int count_letters(string t);
+
+
 int main(void)
 {
 //Your program must prompt the user for a string of text using get_string.
@@ -19,7 +22,7 @@ int count_letters(t)
 
 // a string of text, and that returns an int, the number of letters in that text.
 {
-for (int i = 0, count_letters = strlen(t); i < count_letters; i++)
+for (int i = 0; count_letters = strlen(t); i < count_letters; i++)
 
     printf("%i letters\n", count_letters);
 }
