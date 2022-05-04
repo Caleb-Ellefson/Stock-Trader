8a44e387f127b747cb6264eb8b80c4086422ecaf
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-04 01:02:04 +0000

    Wed, May 4, 2022, 1:02 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -14,7 +14,9 @@ int main(void)
 //If the resulting index number is 16 or higher (equivalent to or greater than a senior undergraduate reading level), your program should output "Grade 16+" instead of giving the exact index number. If the index number is less than 1, your program should output "Before Grade 1".
 
 }
+
 int count_letters(t)
+
 // a string of text, and that returns an int, the number of letters in that text.
 {
 for (int i = 0, count_letters = strlen(t); i < count_letters, i++)
