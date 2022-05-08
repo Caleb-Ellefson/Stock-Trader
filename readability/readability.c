5fec11f5adfd57493991300b94d98a6c0299ed83
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-08 15:51:02 +0000

    Sun, May 8, 2022, 3:51 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -22,14 +22,14 @@ int main(void)
 }
 
 // a string of text, and that returns an int, the number of letters in that text.
-    int count_letters(string t)
+int count_letters(string t)
+{
+    int count_letters = 0
+    for (int i = 0, n = strlen(t); i < n; i++)
     {
-        int count_letters = 0
-        for (int i = 0, n = strlen(t); i < n; i++)
-        {
-            count_letters++;
-        }
-        return count_letters
+        count_letters++;
+    }
+    return count_letters
     }
 
 int count_sentences(string t)
