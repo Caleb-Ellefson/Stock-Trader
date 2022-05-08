999c889ce08da441aac1bc1ff95235acf86315d6
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-08 17:43:17 +0000

    Sun, May 8, 2022, 5:43 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -14,8 +14,7 @@ int main(void)
         string t = get_string ("Text: ");
         printf("%s\n", t);
 
-
-        
+    if ind
     }
 //If the resulting index number is 16 or higher (equivalent to or greater than a senior undergraduate reading level), your program should output "Grade 16+" instead of giving the exact index number. If the index number is less than 1, your program should output "Before Grade 1".
 
