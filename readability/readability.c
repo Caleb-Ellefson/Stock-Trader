c5f028668b599d1a474df6b93cd8d0b91efcf662
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-08 17:38:25 +0000

    Sun, May 8, 2022, 5:38 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -20,7 +20,8 @@ int main(void)
 //Your program should print as output "Grade X" where X is the grade level computed by the Coleman-Liau formula, rounded to the nearest integer.
 float l (string t, int count_words, int count_sentences, int count_letters)
 {
-    float l == (count_words / 100);
+    int count_letters
+    float l == (count_letters / count_words);
     float s == (count_setences / count_words);
 }
 
