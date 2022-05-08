e837ed7771568f0f0a35a98b36fd767778f1a3b9
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-08 16:19:06 +0000

    Sun, May 8, 2022, 4:19 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -37,7 +37,7 @@ int count_sentences(string t)
     int count_sentences = 0;
     for (int i = 0, n = strlen(t); i < n; i++)
     {
-        if (t[i] == '!' t[i] == '.' t[i] == '?')
+        if t[i] == '!' t[i] == '.' t[i] == '?'
         {
             count_sentences++;
         }
