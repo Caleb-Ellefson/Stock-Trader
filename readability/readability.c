f0c4f864b9b8bc05e532616c5d4bc339e85384d7
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-08 19:58:29 +0000

    Sun, May 8, 2022, 7:58 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -11,53 +11,46 @@ int index (string t, int count_words, int count_sentences, int count_letters);
 
 int main(void)
 {
-int index;
     {
         //prompt the user for a string of text using get_string.
         string t = get_string ("Text: ");
     }
 
-    if (index >= 16)
-    {
-    //your program should output "Grade 16+"
-        printf("Grade 16+");
-    }
-    else if (index < 1)
-    {
-        printf("Before Grade 1\n");
-    }
-    else
-    {
-        printf("Grade %i\n", (int)round(index));
-    }
 
-}
 //Your program should print as output "Grade X" where X is the grade level computed by the Coleman-Liau formula, rounded to the nearest integer.
-int index (string t, int count_words, int count_sentences, int count_letters)
-{
+int index (string t, int count_words, int count_sentences, int count_letters);
+
     float l = (float) count_letters / count_words;
     float s = (float) count_sentences / count_words;
-    int index = round(0.0588 * l - 0.296 * s - 15.8);
+    int index = round(0.0588 * l - 0.296 * s - 15.8)
 
-
-    return index;
-}
+        if (index >= 16)
+        {
+            //your program should output "Grade 16+"
+            printf("Grade 16+");
+        }
+        else if (index < 1)
+        {
+            printf("Before Grade 1\n");
+        }
+        else
+        {
+            printf("Grade %i\n", (int)round(index));
+        }
 
 
 //Calculate how many letters are in the string.
 int count_letters(string t)
-{
+
     int count_letters = 0;
     for (int i = 0, n = strlen(t); i < n; i++)
     {
         count_letters++;
     }
-    return count_letters;
 
-    }
 //Calculate how many sentences are in the string.
 int count_sentences(string t)
-{
+
     int count_sentences = 0;
     for (int i = 0, n = strlen(t); i < n; i++)
     {
@@ -66,11 +59,10 @@ int count_sentences(string t)
             count_sentences++;
         }
     }
-        return count_sentences;
-}
+
 //Calculate how many words are in the string.
 int count_words(string t)
-{
+
     int count_words = 1;
     for (int i = 0, n = strlen(t); i < n; i++)
     {
@@ -79,5 +71,5 @@ int count_words(string t)
             count_words++;
         }
     }
-    return count_words;
+
 }
