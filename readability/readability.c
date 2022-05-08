809d717f30c863a574425ce87bf206f4923341e6
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-08 16:27:40 +0000

    Sun, May 8, 2022, 4:27 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -14,14 +14,13 @@ int main(void)
         string t = get_string ("Text: ");
         printf("%s\n", t);
     }
-//our program should count the number of letters, words, and sentences in the text.
-// any sequence of characters separated by spaces should count as a word and that any occurrence of a period, exclamation point, or question mark indicates the end of a sentence.
-//Your program should print as output "Grade X" where X is the grade level computed by the Coleman-Liau formula, rounded to the nearest integer.
 //If the resulting index number is 16 or higher (equivalent to or greater than a senior undergraduate reading level), your program should output "Grade 16+" instead of giving the exact index number. If the index number is less than 1, your program should output "Before Grade 1".
 
 }
+//Your program should print as output "Grade X" where X is the grade level computed by the Coleman-Liau formula, rounded to the nearest integer.
 
-// a string of text, and that returns an int, the number of letters in that text.
+
+//Calculate how many letters are in the string.
 int count_letters(string t)
 {
     int count_letters = 0;
@@ -31,7 +30,7 @@ int count_letters(string t)
     }
     return count_letters;
     }
-
+//Calculate how many sentences are in the string.
 int count_sentences(string t)
 {
     int count_sentences = 0;
@@ -44,7 +43,7 @@ int count_sentences(string t)
     }
         return count_sentences;
 }
-
+//Calculate how many words are in the string.
 int count_words(string t)
 {
     int count_words = 0;
