8f6df95c612392d04761ca0626e5891933375bdd
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-08 20:06:17 +0000

    Sun, May 8, 2022, 8:06 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -22,29 +22,30 @@ int main(void)
     int index (int count_words, int count_sentences, int count_letters);
 
     if (index >= 16)
-    {
+
     //your program should output "Grade 16+"
         printf("Grade 16+");
-    }
+
     else if (index < 1)
-    {
+
         printf("Before Grade 1\n");
-    }
+
     else
-    {
-        printf("Grade %i\n",(int) round(index);
-    }
+
+        printf("Grade %i\n",(int) round(index));
+
 
 }
+
 //Your program should print as output "Grade X" where X is the grade level computed by the Coleman-Liau formula, rounded to the nearest integer.
 int index (string t, int count_words, int count_sentences, int count_letters)
 {
     float l = (float) count_letters / count_words;
     float s = (float) count_sentences / count_words;
     int index = round(0.0588 * l - 0.296 * s - 15.8);
+
     return index;
 }
-
 
 //Calculate how many letters are in the string.
 int count_letters(string t)
@@ -57,6 +58,7 @@ int count_letters(string t)
     return count_letters;
 
     }
+
 //Calculate how many sentences are in the string.
 int count_sentences(string t)
 {
@@ -70,6 +72,7 @@ int count_sentences(string t)
     }
         return count_sentences;
 }
+
 //Calculate how many words are in the string.
 int count_words(string t)
 {
