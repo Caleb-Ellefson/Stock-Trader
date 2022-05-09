4d4f416a2a666c8ca3ee74abc6d4fa07109d8153
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-09 00:47:36 +0000

    Mon, May 9, 2022, 12:47 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -6,21 +6,15 @@ bool only_digits(string s);
 int main(int argc, string argv[])
 {
 // only 2 inputs on the command line
-    if (argc > 2 || argc < 2)
+    if (argc != 2 || !only_digits(argv[1]))
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
-
-
-    if only_digits(agrv[1]) == false
-        (
-            printf("Usage: ./caesar key\n");
-            return 1;
-        )
     else
+    {
         return 0;
-
+    }
     // Make sure every character in argv[1] is a digit
 
     // Convert argv[1] from a `string` to an `int`
