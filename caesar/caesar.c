833c5fdf7bfdd1b5926a509491199985ddac7ed9
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-10 17:44:46 +0000

    Tue, May 10, 2022, 5:44 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -9,7 +9,6 @@ char rotate(char p, int k);
 
 int main(int argc, string argv[])
 {
-    int k;
 
     if (argc != 2 || !only_digits(argv[1]))
     {
@@ -18,10 +17,18 @@ int main(int argc, string argv[])
     }
 
     // Change argv[1] int an int
-    k = atoi(argv[1]);
+    int k = atoi(argv[1]);
 
-    string plaintext = get_string("plaintext: ");
+    string plain = get_string("plaintext: ");
+    int length = strlen(plain);
+    char cypher[length+1];
+    for(int i = 0; i < length; i++)
+    {
+        cypher[i] = roate(plain[i]);
+    }
+    cypher[i] = '\0';
 
+    printf("ciphertext: %s", cypher);
 }
 
 bool only_digits(string s)
@@ -37,6 +44,22 @@ bool only_digits(string s)
 
 char rotate(char p, int k)
 {
-    c = (p + k)%26;
-    
+    char c;
+
+    if(isupper(p))
+    {
+        c = (p - 'A' + k)%26 + 'A';
+    }
+
+    else if(islower(p))
+    {
+        c = (p - 'a' + k)%26 + 'a';
+    }
+
+    else
+    {
+        c = p;
+    }
+
+    return c;
 }
