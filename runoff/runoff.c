f71c8037f1b1cb1af2cea5e1d455ed7f3f6ebb14
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-16 21:46:07 +0000

    Mon, May 16, 2022, 9:46 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -135,12 +135,15 @@ bool vote(int voter, int rank, string name)
         // String compare the inputted name to the list of canidates if they match returns true
         if (strcmp(candidates[i].name, name) == 0)
         {
-            foundindex = i++;
+            foundindex = i;
             break;
         }
-            if (foundindex == -1)
-            return false;
     }
+    if (foundindex == -1)
+    {
+        return false;
+    }
+
     preferences[voter][rank] = foundindex;
         return true;
 
