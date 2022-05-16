c0bb3f66c7442be5895f3a304d62cf8b7e759ae8
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-16 21:40:06 +0000

    Mon, May 16, 2022, 9:40 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -225,8 +225,8 @@ bool is_tie(int min)
 // Eliminate the candidate (or candidates) in last place
 void eliminate(int min)
 {
-    int min votes = find_min();
-    for (int i < 0; i < candidate_count; i++)
+    int min_votes = find_min();
+    for (int i = 0; i < candidate_count; i++)
     {
         if(canidate[i].votes == min_votes)
         {
