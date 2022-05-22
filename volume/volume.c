22c2dbde5b2e1ff55c391e5536f281d1ddd94c48
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-22 19:06:54 +0000

    Sun, May 22, 2022, 7:06 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
 
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
-    fread(&buffer, )
+    fread(&buffer,  )
     // Close files
     fclose(input);
     fclose(output);
