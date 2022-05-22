565b3fa26bb5ab2ac1695cb758a23b068d137d9b
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-22 23:57:11 +0000

    Sun, May 22, 2022, 11:57 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,9 +3,17 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    float red = image[][].rgbtRed;
+    float green = image[][].rgbtGreen;
+    float blue = image [][].rgbtBlue;
+
+
     while (int i = 0; i < image[height - 1], i++)
     {
         while (int j = 0; j < image[width], j++)
+        {
+            red
+        }
     }
     //create a loop that repeats for each row in the image
     // create a loop that repeats for each pixel the in the row
