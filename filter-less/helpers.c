78a0fd915f46d041f400d522675cd404d1f04333
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-23 13:04:02 +0000

    Mon, May 23, 2022, 1:04 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -27,7 +27,16 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+    int orginalRed = image[][].rgbtRed
+    for (int i = 0; i < height, i++)
+    {
+        for (int j = 0; j < width, j++)
+        {
+              sepiaRed = .393 * image[i][j].rgbtRed + .769 * originalGreen + .189 * originalBlue
+              sepiaGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue
+              sepiaBlue = .272 * originalRed + .534 * originalGreen + .131 * originalBlue
+        }
+    }
     return;
 }
 
