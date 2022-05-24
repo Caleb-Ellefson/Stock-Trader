28f23bd1a1f5e1cdcee095847db5a5d6019fede9
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-24 19:53:30 +0000

    Tue, May 24, 2022, 7:53 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -20,7 +20,8 @@ int main(int argc, char *argv[])
         printf("File could not be opened.\n");
     }
     BYTE buffer[BLOCK_SIZE];
-    
+
+    while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
  // Repeat until end of card
     //Read 512 bytes into buffer
     // If start of new JPEG
