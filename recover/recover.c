7f993a5ffad539595a74918021fb9cd6e6c03e95
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-24 19:32:02 +0000

    Tue, May 24, 2022, 7:32 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    FILE *file = fopen(argv[1], "r");
+    CARD.RAW *card.raw = fopen(argv[1], "r");
  // Open the memory card
  // Repeat until end of card
     //Read 512 bytes into buffer
