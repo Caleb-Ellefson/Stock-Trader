079ea5594bb24b960363ad6ac0b2356e06cf9733
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-24 19:23:35 +0000

    Tue, May 24, 2022, 7:23 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+ // Open the memory card
+ // Repeat until end of card
+    //Read 512 bytes into buffer
+    // If start of new JPEG
+        //If first JPEG
+        //Else close old and open new file
+    //Else
+        //If already found a JPEG
+
 }
