4ae62eb283b2caa9e8ec0d72de00181b3392097c
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-29 18:29:52 +0000

    Sun, May 29, 2022, 6:29 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -7,10 +7,27 @@
 typedef uint8_t BYTE;
 int JPEG_Count = 0;
 
+int inputError()
+{
+    printf("Usage: ./recover IMAGE\n");
+    return 1;
+}
+
 int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        printf("INVAILD\n");
-        return 1;
+        return inputError();
     }
+
+    char* inputFile = argv[1];
+    if(inputFile == NULL)
+    {
+        return inputError();
+    }
+
+
+
+
+    return 0;
+}
