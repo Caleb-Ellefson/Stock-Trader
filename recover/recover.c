7a178b31c7559d6dcab21fc816197e9099c5e0b9
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-29 13:59:29 +0000

    Sun, May 29, 2022, 1:59 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -24,11 +24,11 @@ int main(int argc, char *argv[])
     //check if the file is a JPEG
     if (bytes[0] == 0xff && bytes[1] == 0xd8 && bytes[2] == 0xff && (bytes[3] == 0xf0) == 0xe0)
     {
-        //If first JPEG create a new file
+        //If first JPEG
         if (JPEG_Count == 0)
         {
             sprintf(filename, "%03i.jpg", JPEG_Count);
-            FILE *img = fopen(filename, "w")
+            FILE *img = fopen(filename, "w");
             fwrite(buffer, BLOCK_SIZE, sizeof(uint8_t), output);
             JPEG_Count++;
         }
