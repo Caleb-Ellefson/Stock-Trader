dcc89efe4fd08cd7a1c303d19dda689020e96d4d
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-29 14:17:12 +0000

    Sun, May 29, 2022, 2:17 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -7,13 +7,12 @@
 typedef uint8_t BYTE;
 int JPEG_Count = 0;
 char *filename[];
-FILE *img = fopen(filename, "w");
 int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
         printf("INVAILD\n");
-        return 1
+        return 1;
     }
 
     // Create a buffer the size of a JPEG
@@ -23,7 +22,7 @@ int main(int argc, char *argv[])
     FILE *file = fopen(argv[1], "r");
 
     //check if the file is a JPEG
-    if (bytes[0] == 0xff && bytes[1] == 0xd8 && bytes[2] == 0xff && (bytes[3] == 0xf0) == 0xe0)
+    if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] == 0xf0) == 0xe0)
     {
         //If first JPEG
         if (JPEG_Count == 0)
