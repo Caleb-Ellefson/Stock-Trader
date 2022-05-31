b03c86e5f3e5280a22e463c1cb41f01b7f03dc05
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-31 16:13:09 +0000

    Tue, May 31, 2022, 4:13 PM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -40,7 +40,11 @@ int main(void)
 person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
-    
+    person *n = malloc(sizeof(person));
+    if (person == null)
+    {
+        return 1;
+    }
     // If there are still generations left to create
     if (generations > 1)
     {
