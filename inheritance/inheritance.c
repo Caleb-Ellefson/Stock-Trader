b70a28768a6c9561d58c46771d6254cd82a3720d
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-05-31 19:37:44 +0000

    Tue, May 31, 2022, 7:37 PM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -54,7 +54,8 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        parent0-> 
+        parent0-> person = NULL;
+        parent1-> person = NULL;
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
 
     }
