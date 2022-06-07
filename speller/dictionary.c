64abaa8187f077ff4931a8577cc999ed9ca37743
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-06-07 15:43:19 +0000

    Tue, Jun 7, 2022, 3:43 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -107,5 +107,17 @@ unsigned int size(void)
 bool unload(void)
 {
     // TODO
-    return false;
+    for(int i = 0; i < N; i++)
+    {
+        node* nodePtr = table[i];
+
+        while(nodePtr != NULL)
+        {
+            node* deleteme = nodePtr;
+            nodePtr = nodePtr->next;
+            free(deleteme);
+        }
+        table[i];
+    }
+    return true;
 }
