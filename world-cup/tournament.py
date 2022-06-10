dd2dbab1d647fd83a2680682b1aa5d4a4a06f3c5
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-06-10 01:55:18 +0000

    Fri, Jun 10, 2022, 1:55 AM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -16,7 +16,10 @@ def main():
 
     teams = []
     # TODO: Read teams into memory from file
-
+    with open("2018m.csv", "r") as file:
+        next (reader)
+        for row in reader:
+            teams = 1
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
 
