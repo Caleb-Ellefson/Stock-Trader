c86ef704ebf9834d33b66d9dbae575e9fc8168ba
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-06-10 02:31:02 +0000

    Fri, Jun 10, 2022, 2:31 AM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -36,7 +36,7 @@ def main():
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
     for i in range(n):
-        winner = simulate_torunament(teams)
+        winner = simulate_tournament(teams)
         if winner in counts:
             counts[winner] += 1
         else:
