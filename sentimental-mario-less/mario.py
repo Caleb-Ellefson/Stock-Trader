f79942e77de4269fc4400cebde6b050fd0512a54
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-06-13 16:55:53 +0000

    Mon, Jun 13, 2022, 4:55 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -3,7 +3,12 @@ from cs50 import get_string
 # Get_int for int between 1 and 8
 h = get_string ("Height: ")
 
-if h < 1 or h > 8
+if (h < 1 or h > 8)
 {
     print("Invaild Height")
+}
+
+else
+{
+    
 }
