4da17a13260467572cc130e203a22dd8ba98489d
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-06-14 16:20:02 +0000

    Tue, Jun 14, 2022, 4:20 PM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,1 +1,14 @@
 # TODO
+while True:
+    h = get_int ("Height: ")
+    if h >= 1 and h <=8:
+        break
+
+for i in range(h):
+    for j in range(h):
+        if i + j >= h - 1:
+            print("#", end="")
+        else:
+            print(" ", end="")
+    print()
+
