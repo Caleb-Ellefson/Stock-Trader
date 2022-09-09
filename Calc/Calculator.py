be52a62b1d78ee627d8741c853ebae1cd5ebb8ba
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-09-09 01:22:36 +0000

    ddd, Sep 9/9/2022, YYYY, 1:22 A +0

--- Calc/Calculator.py
+++ Calc/Calculator.py
@@ -4,3 +4,7 @@ PAY_RATE = 7.25
 TOTAL_PAY = (HOURS_WORKED * PAY_RATE)
 
 print(TOTAL_PAY)
+
+def ovetime_pay(HOURS_WORKED, PAY_RATE):
+    overtime = (HOURS_WORKED - 40)
+    ((PAY_RATE * 1.5) * overtime) = x + 290
