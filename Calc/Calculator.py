8fed333251c9ed7479c04a4954e4814840cd1eeb
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-09-09 01:29:53 +0000

    ddd, Sep 9/9/2022, YYYY, 1:29 A +0

--- Calc/Calculator.py
+++ Calc/Calculator.py
@@ -6,5 +6,15 @@ TOTAL_PAY = (HOURS_WORKED * PAY_RATE)
 print(TOTAL_PAY)
 
 def ovetime_pay(HOURS_WORKED, PAY_RATE, TOTAL_PAY):
-    overtime = (HOURS_WORKED - 40)
-    ((PAY_RATE * 1.5) * overtime) = x + 290
+
+    #Enter function if employee has over 40 hours
+    if (HOURS_WORKED > 40):
+        #Find OT
+        overtime = (HOURS_WORKED - 40)
+        #40 hours of 7.25 is 290 + 1.5 per hour of OT
+        ((PAY_RATE * 1.5) * overtime) = TOTAL_PAY + 290
+        return TOTAL_PAY
+
+
+    else:
+        return(1)
