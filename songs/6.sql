a1bb8b669e0f03f99a7202a09b68736062a351f3
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-09-12 14:54:58 +0000

    ddd, Sep 9/12/2022, YYYY, 2:54 A +0

--- songs/6.sql
+++ songs/6.sql
@@ -1,2 +1,2 @@
-
+SELECT id FROM artists WHERE name = 'Post Malone';
 Select * FROM songs WHERE artist_id = '54';
--- songs/7.sql
+++ songs/7.sql
@@ -1,1 +1,2 @@
+SELECT id FROM artists WHERE name = 'Drake';
 SELECT AVG(energy) FROM songs WHERE artist_id = '23';
