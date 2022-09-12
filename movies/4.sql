12952aa1a6d433ac961c9f1227a998c7b8563b1a
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-09-12 18:49:12 +0000

    Mon, Sep 12, 2022, 6:49 PM +00:00

--- movies/4.sql
+++ movies/4.sql
@@ -1,4 +1,2 @@
-SELECT movie_id FROM ratings WHERE rating = '10';
-SELECT title FROM movies WHERE movie_id FROM ratings = '10'
-Select name FROM songs WHERE artist_id = (SELECT id FROM artists WHERE name = 'Post Malone');
-SELECT title FROM movies WHERE * = (SELECT * FROM ratings WHERE rating = '10');
+
+SELECT title FROM movies WHERE id = (SELECT * FROM ratings WHERE rating = '10');
