72cb0b5f0bc496968c09adabda27296078f0088f
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-09-12 19:07:13 +0000

    Mon, Sep 12, 2022, 7:07 PM +00:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,3 +1,2 @@
 SELECT AVG(energy) FROM songs WHERE artist_id = (SELECT id FROM artists WHERE name = 'Drake');
 SELECT COUNT(title) FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE rating == 10;
-SELECT AVG(rating) FROM ratings JOIN movies ON 
