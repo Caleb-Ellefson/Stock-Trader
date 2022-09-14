a453ed5bea54012116b24d3e0fb643d486c66413
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-09-14 02:52:48 +0000

    Wed, Sep 14, 2022, 2:52 AM +00:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,4 +1,4 @@
-SELECT ASC(rating) FROM ratings
-JOIN movies ON rating.movie_id = movies.id
-SELECT title, year FROM movies
+SELECT movies.title, ratings.rating FROM movies
+JOIN ratings ON ratings.movie_id = movies.id
 WHERE year = 2010
+ORDER BY rating DESC, title DESC;
