29fb14433f66366cabaad0fe44a21a6e8c595778
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-09-14 02:57:08 +0000

    Wed, Sep 14, 2022, 2:57 AM +00:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,2 +1,3 @@
-SELECT title, year FROM movies WHERE year = 2010
-JOIN ratings ON movies
+SELECT movies.id, movies.title, stars.person_id FROM movies
+JOIN stars ON stars.movie_id = movies.id
+WHERE title = 'Toy Story';
