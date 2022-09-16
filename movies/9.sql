c925a16f239667dd2d56290ba9d98766d86d5557
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-09-16 04:34:51 +0000

    Fri, Sep 16, 2022, 4:34 AM +00:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,5 +1,10 @@
 SELECT name FROM people
 JOIN stars ON stars.person_id = person_id
-JOIN movies on stars.movie_id = movies.id
+JOIN movies ON stars.movie_id = movies.id
+WHERE movies.year = 2004
+ORDER BY people.birth;
+SELECT name FROM people
+JOIN stars ON stars.person_id = people.id
+JOIN movies ON stars.movie_id = movies.id
 WHERE movies.year = 2004
 ORDER BY people.birth
