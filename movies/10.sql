fc29b6d10b7631d66fa894b7fd2e460ff789b20b
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-09-16 04:50:45 +0000

    Fri, Sep 16, 2022, 4:50 AM +00:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,4 +1,4 @@
-SELECT person_id FROM directors
-JOIN directors on person.id = directors.person_id
+SELECT name FROM people
+JOIN directors on people.id = directors.person_id
 JOIN ratings on directors.movie_id = ratings.movie_id
 WHERE ratings.rating >= 9.0;
