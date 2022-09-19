392c6912be6f576433b068d13adade6431c5d053
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-09-19 15:11:16 +0000

    Mon, Sep 19, 2022, 3:11 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -12,3 +12,6 @@ WHERE month = 7 AND day = 28 AND hour = 10 AND minute = 15;
 
 -- ID = 258 Entered the store @ 10:08 License_plate = R3G7486
 -- ID = 259 @ 10:14 License_plate = 13FNH73
+SELECT *
+FROM interviews
+WHERE year = 2021 AND month = 7 AND day = 28;
