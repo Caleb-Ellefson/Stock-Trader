624acd58b04de4bfee087e95629eebe80a908967
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-09-19 15:00:10 +0000

    Mon, Sep 19, 2022, 3:00 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -2,4 +2,7 @@
 SELECT description
 FROM crime_scene_reports
 WHERE month = 7 AND day = 28
-AND street = "Chamberlin Street";
+AND street = "Humphrey Street";
+
+-- 10:15am @ humphrey Street bakery 3 Witnesses
+
