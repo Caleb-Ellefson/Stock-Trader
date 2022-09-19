2321bfcbc97a581403efe519998eb59c301770c1
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-09-19 15:16:06 +0000

    Mon, Sep 19, 2022, 3:16 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -15,3 +15,14 @@ WHERE month = 7 AND day = 28 AND hour = 10 AND minute = 15;
 SELECT *
 FROM interviews
 WHERE year = 2021 AND month = 7 AND day = 28;
+
+-- Partner bought the first flight out of Fiftyville for 7/29/2021
+--  | 260 | 2021 | 7     | 28  | 10   | 16     | exit     | 5P2BI95       |
+--  | 261 | 2021 | 7     | 28  | 10   | 18     | exit     | 94KL13X       |
+--  | 262 | 2021 | 7     | 28  | 10   | 18     | exit     | 6P58WS2       |
+--  | 263 | 2021 | 7     | 28  | 10   | 19     | exit     | 4328GD8       |
+--  | 264 | 2021 | 7     | 28  | 10   | 20     | exit     | G412CB7       |
+--  | 265 | 2021 | 7     | 28  | 10   | 21     | exit     | L93JTIZ       |
+--  | 266 | 2021 | 7     | 28  | 10   | 23     | exit     | 322W7JE       |
+--  | 267 | 2021 | 7     | 28  | 10   | 23     | exit     | 0NTHK55       |
+--  | 268 | 2021 | 7     | 28  | 10   | 35     | exit     | 1106N58       |
