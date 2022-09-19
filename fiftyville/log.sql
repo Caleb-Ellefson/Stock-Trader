a0136c9d50b1d97cf6af93e1272aad81772a0fcf
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-09-19 15:08:46 +0000

    Mon, Sep 19, 2022, 3:08 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -9,3 +9,6 @@ AND street = "Humphrey Street";
 SELECT *
 FROM bakery_security_logs
 WHERE month = 7 AND day = 28 AND hour = 10 AND minute = 15;
+
+-- ID = 258 Entered the store @ 10:08 License_plate = R3G7486
+-- ID = 259 @ 10:14 License_plate = 13FNH73
