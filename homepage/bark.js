e4c36097b02765da1a5065c67820b793b99e931c
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-09-26 17:26:33 +0000

    Mon, Sep 26, 2022, 5:26 PM +00:00

--- homepage/bark.js
+++ homepage/bark.js
@@ -1,4 +1,10 @@
 function bark() {
- var img = document.getElementById("intro_subheader");
- if (x.innerHTML ===)
+ var image = document.getElementById("picRight");
+ if (image.scr.match("/Kora/Sitting.png")) {
+    image.src = "/Kora/barking-removebg-preview.png";
+ }
+ else {
+    image.src = "/Kora/Sitting.png";
+ }
+
 }
