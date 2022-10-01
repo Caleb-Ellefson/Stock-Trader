15d2ce8d3351ba4964233718d0ae1ab68ce6f078
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-10-01 03:50:01 +0000

    Sat, Oct 1, 2022, 3:50 AM +00:00

---
+++ NASA/script.js
@@ -0,0 +1,8 @@
+<script>
+    window.addEventListener('scroll', function (){
+        const parallax = document.querySelector('.parallax');
+        let scrollPosition = window.pageYOffset;
+
+        console.log(scrollPosition);
+    })
+</script>
