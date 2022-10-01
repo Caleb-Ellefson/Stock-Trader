5f126616ed5c41cfb069457de1b8b9f4fcaa716f
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-10-01 04:13:42 +0000

    Sat, Oct 1, 2022, 4:13 AM +00:00

--- NASA/script.js
+++ NASA/script.js
@@ -4,5 +4,5 @@
         let scrollPosition = window.pageYOffset;
 
         console.log(scrollPosition);
-        parallax.style.transform = 'translateY(' + scrollPosition * .5 + 'px)';
+        parallax.style.transform = 'translateY(' + scrollPosition * .8 + 'px)';
     });
