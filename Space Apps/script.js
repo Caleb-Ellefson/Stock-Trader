3b1432f6bd13ae3ac54145156c46fad82dcb9b7c
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-10-02 02:43:51 +0000

    Sun, Oct 2, 2022, 2:43 AM +00:00

--- Space Apps/script.js
+++ Space Apps/script.js
@@ -1,10 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-    <meta name="viewport" content="with=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <script src="script.js"></script>
-<section class="chart">
-
-</section>
-
-</html>
+const data = {
+    labels: [
+      'Specific Energy',
+      'Specific Power',
+      'Safety',
+      'Performance',
+      'Life Span',
+      'Cost',
+    ],
+    datasets: [{
+      label: 'Lithium-Nickel-Cobalt-Aluminium (NCA)',
+      data: [100, 100, 30, 90, 95, 50],
+      fill: true,
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      borderColor: 'rgb(255, 99, 132)',
+      pointBackgroundColor: 'rgb(255, 99, 132)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgb(255, 99, 132)'
+    }, {
+      label: 'My Second Dataset',
+      data: [28, 48, 40, 19, 96, 27, 100],
+      fill: true,
+      backgroundColor: 'rgba(54, 162, 235, 0.2)',
+      borderColor: 'rgb(54, 162, 235)',
+      pointBackgroundColor: 'rgb(54, 162, 235)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgb(54, 162, 235)'
+    }]
+  };
