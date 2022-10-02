827f7fad399f5fa258888b5040e14761bd499de6
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-10-02 16:24:58 +0000

    Sun, Oct 2, 2022, 4:24 PM +00:00

--- Space/script.js
+++ Space/script.js
@@ -1,3 +1,17 @@
+const config = {
+  type: 'radar',
+  data: data,
+  options: {
+    elements: {
+      line: {
+        borderWidth: 3
+      }
+    }
+  },
+};
+
+
+
 const data = {
     labels: [
       'Specific Energy',
@@ -17,16 +31,6 @@ const data = {
       pointBorderColor: '#fff',
       pointHoverBackgroundColor: '#fff',
       pointHoverBorderColor: 'rgb(255, 99, 132)'
-    }, {
-      label: 'My Second Dataset',
-      data: [28, 48, 40, 19, 96, 27, 100],
-      fill: true,
-      backgroundColor: 'rgba(54, 162, 235, 0.2)',
-      borderColor: 'rgb(54, 162, 235)',
-      pointBackgroundColor: 'rgb(54, 162, 235)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgb(54, 162, 235)'
     }]
   };
 
