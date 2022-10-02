7a262d7d50d3ed7d68678a55e40318992515d9a3
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-10-02 02:40:03 +0000

    Sun, Oct 2, 2022, 2:40 AM +00:00

--- Space Apps/script.js
+++ Space Apps/script.js
@@ -2,4 +2,9 @@
 <html lang="en">
     <meta name="viewport" content="with=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+<section class="chart">
+
+</section>
+
 </html>
