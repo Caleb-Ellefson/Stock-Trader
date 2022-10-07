942a8284e879e8580c84a9fb3f313d1345e6f382
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-10-07 03:04:35 +0000

    Fri, Oct 7, 2022, 3:04 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -47,13 +47,13 @@ def index():
     user_id = session["user_id"]
 
     purchases_db = db.execute("SELECT SUM(PRICE) FROM purchases WHERE user_id = ?", user_id)
-    #total = purchases_db[1]["price"]
+    total = purchases_db[0]
     cash_db = db.execute("SELECT cash FROM users WHERE id = ?", user_id)
     cash = cash_db[0]["cash"]
     stocks_db = db.execute("SELECT * FROM purchases WHERE id = ? ORDER BY date", user_id)
 
     if request.method == "GET":
-        return render_template("index.html", purchases=stocks_db, cash=cash, total=purchases_db)
+        return render_template("index.html", purchases=stocks_db, cash=cash, total=total)
 
 
 
