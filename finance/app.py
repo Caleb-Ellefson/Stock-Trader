e81fffce15673f90c9282ba9c341d35dbf2e527e
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-10-09 00:58:38 +0000

    Sun, Oct 9, 2022, 12:58 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -51,7 +51,7 @@ def index():
     purchases_db = db.execute("SELECT symbol, SUM(SHARES) AS shares, price FROM purchases WHERE user_id = ? GROUP BY symbol", user_id)
 
     #total amount of stocks purchased
-    total = db.execute("SELECT SUM(price) AS "total" FROM purchases WHERE user_id = ?", user_id)
+    total = db.execute("SELECT SUM(price) AS total FROM purchases WHERE user_id = ?", user_id)
 
 
 
