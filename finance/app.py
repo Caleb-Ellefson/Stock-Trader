4f3a234aa91aea99cba321682f3162875075d717
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-10-10 00:33:30 +0000

    Mon, Oct 10, 2022, 12:33 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -126,8 +126,11 @@ def buy():
 @app.route("/history")
 @login_required
 def history():
-    """Show history of transactions"""
-    return apology("TODO")
+
+    if request.method == "GET":
+        return render_template("history.html")
+
+
 
 
 @app.route("/login", methods=["GET", "POST"])
