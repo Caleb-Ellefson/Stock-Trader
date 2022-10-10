d69737b1e7324c4797b8b7fc06cbb79f188478a1
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-10-10 00:23:45 +0000

    Mon, Oct 10, 2022, 12:23 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -265,15 +265,11 @@ def sell():
         #Select the symbol of stock add the shares togther and seperate the sum of shares by symbol
         purchases_db = db.execute("SELECT symbol, SUM(SHARES) AS shares, price FROM purchases WHERE user_id = ? GROUP BY symbol", user_id)
 
-        #total amount of stocks purchased
-        total = db.execute("SELECT SUM(price) AS total FROM purchases WHERE user_id = ?", user_id)
-        total = total[0]['total']
-
         #find users cash
         cash_db = db.execute("SELECT cash FROM users WHERE id = ?", user_id)
         cash = round(cash_db[0]["cash"], 2)
 
-        return render_template("sell.html", database=purchases_db, cash=cash, total=total)
+        return render_template("sell.html", database=purchases_db, cash=cash)
 
     else:
         symbol = request.form.get("symbol")
