1691320aa5e48fb9be45c182e29bb2d9159b4265
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-10-10 00:11:43 +0000

    Mon, Oct 10, 2022, 12:11 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -302,7 +302,7 @@ def sell():
 
         date = datetime.datetime.now()
 
-        db.execute("INSERT INTO purchases (symbol, shares, price, date, user_id) VALUES (?, ?, ?, ?, ?)", stock["symbol"], quantity, stock["price"], date, user_id)
+        db.execute("INSERT INTO purchases (symbol, shares, price, date, user_id) VALUES (?, ?, ?, ?, ?)", stock["symbol"], (-1)*quantity, stock["price"], date, user_id)
 
         flash("Sold!")
 
