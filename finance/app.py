1517596daa234c6d45eb572c9568bb0725b771ac
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-10-24 18:11:36 +0000

    Mon, Oct 24, 2022, 6:11 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -48,7 +48,7 @@ def index():
     user_id = session["user_id"]
 
     #Select the symbol of stock add the shares togther and seperate the sum of shares by symbol
-    purchases_db = db.execute("SELECT symbol, SUM(SHARES) AS shares, price FROM purchases WHERE user_id = ? AND shares > 0 GROUP BY symbol", user_id)
+    purchases_db = db.execute("SELECT symbol, SUM(SHARES) AS shares, price FROM purchases WHERE user_id = ? GROUP BY symbol", user_id)
     print(purchases_db)
 
     #total amount of stocks purchased
@@ -282,7 +282,7 @@ def sell():
         user_id = session["user_id"]
 
         #Select the symbol of stock add the shares togther and seperate the sum of shares by symbol
-        purchases_db = db.execute("SELECT symbol, SUM(SHARES) AS shares, price FROM purchases WHERE user_id = ? AND shares >= 1 GROUP BY symbol", user_id)
+        purchases_db = db.execute("SELECT symbol, SUM(SHARES) AS shares, price FROM purchases WHERE user_id = ? GROUP BY symbol", user_id)
 
         #find users cash
         cash_db = db.execute("SELECT cash FROM users WHERE id = ?", user_id)
