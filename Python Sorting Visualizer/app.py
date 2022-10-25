8f7903cb795bb1f1573130c2e906a3b7f801ca64
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-10-25 17:24:28 +0000

    Tue, Oct 25, 2022, 5:24 PM +00:00

--- Python Sorting Visualizer/app.py
+++ Python Sorting Visualizer/app.py
@@ -33,7 +33,6 @@ class DrawInformation:
         self.block_width = round((self.width - self.SIDE_PAD) / len(lst))
         #Find the height of each bar
         self.block_height = round((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
-
         #int divide to get a whole number
         self.start_x = self.SIDE_PAD // 2
 
