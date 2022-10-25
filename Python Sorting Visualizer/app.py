ce1b43b23682220791db78c5c2eb784ef5acbb03
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-10-25 16:22:23 +0000

    Tue, Oct 25, 2022, 4:22 PM +00:00

--- Python Sorting Visualizer/app.py
+++ Python Sorting Visualizer/app.py
@@ -2,3 +2,17 @@ import pygame
 import random
 pygame.init()
 
+class DrawInformation:
+    BLACK = 0, 0, 0,
+    WHITE = 255, 255, 255
+    GREEN = 0, 255, 0
+    RED = 255, 0, 0
+    GRAY = 128, 128, 128
+    BACKGROUND_COLOR = WHITE
+
+    def __init__(self, width, height, lst):
+        self.width = width
+        self.height = height
+
+        #create the pygame window
+        self.window = pygame.display.set_mode((width, height))
