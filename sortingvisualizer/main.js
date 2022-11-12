26b9d0f0eb0ff10ba09c292deac8d30604691d3e
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-11-12 02:51:36 +0000

    Sat, Nov 12, 2022, 2:51 AM +00:00

---
+++ sortingvisualizer/main.js
@@ -0,0 +1,8 @@
+
+// Finding Size of Array
+let arrsize=document.querySelector('#sizer');
+arrsizer.addEventListener('input', function(){
+    generatebars(parseInt(arrsizer.value))});
+generatebars();
+
+// Finding Sorting Speed
