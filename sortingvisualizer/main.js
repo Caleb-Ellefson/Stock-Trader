5ab28297d1237156977290e80693c5b251c8ed08
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-11-12 16:48:34 +0000

    Sat, Nov 12, 2022, 4:48 PM +00:00

--- sortingvisualizer/main.js
+++ sortingvisualizer/main.js
@@ -79,5 +79,29 @@ function enableSizeSlider(){
 
 
 function bubble (){
-    
+    for (let i=0; i<ele.length-1; i++)
+    {
+        for (j=0; j<ele.length-1-i; j++)
+        {
+            ele[j].style.background='red';
+            ele[j+1].style.background='red';
+
+            if(parseInt(ele[j].style.height) > parseInt(ele[j+1].style.height))
+            {
+                await delay(time);
+                swap(ele[j], ele[j+1]);
+            }
+
+            ele[ele.length-i-1].style.background='green';
+
+        }
+        ele[0].style.background='green';
+    }
+    document.getElementById('bubble').addEventListener("click", async function(){
+        disableSizeSlider();
+        disableSortingBtn();
+        bubble();
+        enableSizeSlider();
+        enableSortingBtn();
+    });
 }
