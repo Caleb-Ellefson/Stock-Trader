40e64e976e1675dc4d4ef622cd781b66c3d924b5
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-11-12 16:27:38 +0000

    Sat, Nov 12, 2022, 4:27 PM +00:00

--- sortingvisualizer/main.js
+++ sortingvisualizer/main.js
@@ -22,3 +22,20 @@ function generatebars(noBar=60){
         bar.push(Math.floor(Math.random()*400)+1);
     }
 }
+
+const divs=document.querySelector('#bar');
+
+for(let x=0; x<noBar; x++)
+    {
+        const temp=document.createElement("div");
+        temp.classList.add('sort');
+        temp.classList.add('Baritem');
+        temp.style.height=bars[x]+"px";
+        divs.append(temp);
+    }
+
+document.getElementById("newarr").addEventListener("click",function(){
+    generateBars(arrsizer.value);
+    enableSortingBtn();
+    enableSizeSlider();
+});
