89e9fb3241de4930749112bfd8af45c306c9f214
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-11-22 17:09:40 +0000

    Tue, Nov 22, 2022, 5:09 PM +00:00

---
+++ IdCard/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import About from "./Componets/About"
+import Footer from "./Componets/Footer"
+import Info from "./Componets/Info"
+import Interests from "./Componets/Interests"
+
+export default function App () {
+    return(
+        <div>
+            <About />
+            <Footer />
+            <Info />
+            <Interests />
+        </div>
+    )
+
+}
---
+++ IdCard/Componets/Info.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Info() {
+    return(
+        <div>
+            <img src="../images/profile.png" alt="profile" width="315" height="315" />
+        </div>
+    )
+}
