9ad1d3a750423b820acf55bf5d8f928b68e0fe8f
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-11-29 20:21:04 +0000

    Tue, Nov 29, 2022, 8:21 PM +00:00

--- my-app/src/components/Meme.js
+++ my-app/src/components/Meme.js
@@ -4,10 +4,10 @@ import React from 'react';
 export default function Meme () {
     return(
         <main className="meme">
-            <form>
+            <form className="form">
                 <input className="meme--input" type="text" placeholder="Top text" />
                 <input className="meme--input" type="text" placeholder="Bottom text" />
-                <button> Get a new meme image</button>
+                <button className="meme--button"> Get a new meme image</button>
             </form>
         </main>
     )
