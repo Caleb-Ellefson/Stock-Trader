6850a3a55e13b1070304940beb32943e256513e2
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-11-29 20:10:05 +0000

    Tue, Nov 29, 2022, 8:10 PM +00:00

--- my-app/src/components/Meme.js
+++ my-app/src/components/Meme.js
@@ -3,8 +3,10 @@ import React from 'react';
 
 export default function Meme () {
     return(
-        <div className="Nav">
-            
+        <div className="meme">
+            <input className="meme--input" type="text" placeholder="Top text"> </input>
+            <input className="meme--input" type="text" placeholder="Bottom text"> </input>
+            <button> </button>
         </div>
     )
 }
