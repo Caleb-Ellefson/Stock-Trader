84ba79d8cfdbd25e927f78cd47afaec2e2c67c14
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-11-29 17:14:26 +0000

    Tue, Nov 29, 2022, 5:14 PM +00:00

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
+
 export default function Header () {
     return(
         <div>
-            <img className="nav--logo" src="../Images/TrollFace.png" alt='logo' />
+            <img className="nav--logo" src="Images/TrollFace.png" alt='logo' />
             <h1 className="nav--title"> Meme Generator</h1>
             <h4 className="nav--course"> React Course - Project 3 </h4>
         </div>
