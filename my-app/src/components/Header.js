011fd91c9d3df3f666f74890a840a281f05a0689
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-11-29 16:59:40 +0000

    Tue, Nov 29, 2022, 4:59 PM +00:00

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -4,6 +4,7 @@ export default function Header () {
     return(
         <div>
             <img src="my-app/Images/TrollFace.png" alt='logo' />
+            <h1> Meme Generator</h1>
         </div>
     )
 }
