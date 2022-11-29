96dbd4e1c5065422f141844c37f8870444e80f10
Lobster <69178718+Caleb-Ellefson@users.noreply.github.com>
2022-11-29 16:12:39 +0000

    Tue, Nov 29, 2022, 4:12 PM +00:00

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header () {
     return(
         <div>
-            <img src="../my-app/src/Images/TrollFace.png" alt='logo' />
+            <img src="../Images/TrollFace.png" alt='logo' />
         </div>
     )
 }
